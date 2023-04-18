6f1a9a78b8956268c5a80dbb106ab5638e219db0
App317 <101300095+App317@users.noreply.github.com>
2023-04-18 17:24:31 -0400

    almost complete
    need to make the hovered item stay at 1 opacity while fading other items

--- src/features/menu/HomeMenu.js
+++ src/features/menu/HomeMenu.js
@@ -3,44 +3,38 @@ import styles from '../../modules/HomeMenu.module.css';
 import { useState } from 'react';
 
 const HomeMenu = () => {
-  const fade = { opacity: 0.3 };
-  const unfade = { opacity: 1.0 };
-  const [textFade, setTextFade] = useState(unfade);
-  const texts = ['about', 'projects', 'resume', 'contact'];
+  const [hoverText, setHoverText] = useState(null);
+  const texts = ['About me', 'Projects', 'Resume', 'Contact Me'];
+  const urls = ['/about', '/projects', '/resume', '/contact'];
+
+  function handleHover(index) {
+    setHoverText(index);
+  }
+  function handleLeave() {
+    setHoverText(null);
+  }
+
+  const opacity = hoverText === null ? 1 : 0.5;
 
   return (
-    <div className={styles.menu}>
-      <div className={styles.menuItems}>
-        <div>
+    <div className={styles.menuItems}>
+      <div>
+        {texts.map((text, index) => (
           <Link
-            style={textFade}
-            onMouseOver={() => setTextFade(fade)}
-            onMouseOut={() => setTextFade(unfade)}
-            className={styles.menuItem}
-            to="/about"
+            key={index}
+            className={`${styles.menuItem} ${
+              index === hoverText || window.location.pathname === urls[index]
+                ? 'active'
+                : ''
+            } ${index === hoverText ? styles.hovered : ''}`}
+            onMouseEnter={() => handleHover(index)}
+            onMouseLeave={() => handleLeave()}
+            to={urls[index]}
+            style={{ opacity }}
           >
-            <h1>About Me</h1>
+            {text}
           </Link>
-        </div>
-        <div>
-          <Link
-            style={textFade}
-            onMouseOver={() => setTextFade(unfade)}
-            onMouseOut={() => setTextFade(fade)}
-            className={styles.menuItem}
-            to="/projects"
-          >
-            <h1>Projects</h1>
-          </Link>
-        </div>
-        <div>
-          <Link className={styles.menuItem} to="/resume">
-            <h1>Resume</h1>
-          </Link>
-          <Link className={styles.menuItem} to="/contact">
-            <h1>Contact me</h1>
-          </Link>
-        </div>
+        ))}
       </div>
     </div>
   );
