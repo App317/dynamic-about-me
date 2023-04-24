94f76bfd6e188344468cea95a72289b9c246fca0
App317 <101300095+App317@users.noreply.github.com>
2023-04-24 16:29:31 -0400

    debugging
    debugging the usage of my background color not being changed without usage of 'important' rule

---
+++ src/components/HomeBackground.js
@@ -0,0 +1,7 @@
+import styles from '../modules/HomeBackground.module.css';
+
+const HomeBackground = () => {
+  return <div className={styles.background}></div>;
+};
+
+export default HomeBackground;
