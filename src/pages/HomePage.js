55f12b2c585977d601aaf491e2ec7b9cba7eb192
App317 <101300095+App317@users.noreply.github.com>
2023-04-25 16:20:40 -0400

    setting up homebackground
    altering it so it doesnt affect the other elements and can make

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import { Container } from 'reactstrap';
 import HomeMenu from '../features/menu/HomeMenu';
+import HomeBackground from '../components/HomeBackground';
 import styles from '../modules/HomePage.module.css';
 
 const HomePage = () => {
   return (
     <Container className={styles.container}>
       <div>{<HomeMenu />}</div>
+      <div> {<HomeBackground />} </div>
     </Container>
   );
 };
