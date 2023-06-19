e30fbc195b149feab9ca1fa625814a1f4c36706c
App317 <101300095+App317@users.noreply.github.com>
2023-06-19 17:52:50 -0400

    save
    just ensuring everything has been saved properly and commited to master branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ProjectsPage from './pages/ProjectsPage';
+import ResumePage from './pages/ResumePage';
+import ContactPage from './pages/ContactPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/*<Header />*/}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <HomePage />
+      <div className="right-column">
+        <Routes>
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
